dab68498658d45ee97fd37a153708f0aee9adc52
Fritz Gerald Castillo <fritzgerald658@gmail.com>
2024-09-11 14:47:21 +0800

    furniro mockup coding

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./PracticeReact.jsx";
 import "./practiceReact.css";
 import "./app.css";
 import PracticeReact from "./PracticeReact.jsx";
+import Nav from "./furniro_components/Nav.jsx";
+import Hero from "./furniro_components/Hero.jsx";
 function App() {
   return (
     <>
-      <PracticeReact />
+      <Nav />
     </>
   );
 }
---
+++ src/furniro_components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <>
+      <div className="container-fluid bg-white d-flex justify-content-between">
+        <div className="logo-container"></div>
+        <nav></nav>
+        <div className="nav-icons-container"></div>
+      </div>
+    </>
+  );
+}
+
+export default Nav;
