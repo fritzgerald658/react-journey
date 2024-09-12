ef78a80bdf90ef3eed5061e06f87f330923e19be
Fritz Gerald Castillo <fritzgerald658@gmail.com>
2024-09-12 14:34:42 +0800

    our products initial card responsiveness almost done

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Hero from "./furniro_components/Hero.jsx";
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 import SectionTwo from "./furniro_components/SectionTwo.jsx";
+import OurProducts from "./furniro_components/OurProducts.jsx";
 function App() {
   return (
     <>
       <Nav />
       <Hero />
       <SectionTwo />
+      <OurProducts />
     </>
   );
 }
--- src/furniro_components/OurProducts.jsx
+++ src/furniro_components/OurProducts.jsx
@@ -1,24 +1,61 @@
 import React from "react";
-import funiroMuggo from "../furniro_assets/furniro-muggo.jpg";
-import funiroLolito from "../furniro_assets/furniro-lolito.jpg";
-import funiroPingky from "../furniro_assets/furniro-pingky.jpg";
-import funiroSyltherine from "../furniro_assets/furniro-syltherine.jpg";
-import funiroPotty from "../furniro_assets/furniro-potty.jpg";
-import funiroGrifo from "../furniro_assets/furniro-grifo.jpg";
-import funiroLeviosa from "../furniro_assets/furniro-leviosa.jpg";
-import funiroRespira from "../furniro_assets/furniro-respira.jpg";
-
-function OurProductsCard({productTitle, productSubtitle, productSalePrice, productOriginalPrice}) {
+import furniroMuggo from "../furniro_assets/furniro-muggo.jpg";
+import furniroLolito from "../furniro_assets/furniro-lolito.jpg";
+import furniroPingky from "../furniro_assets/furniro-pingky.jpg";
+import furniroSyltherine from "../furniro_assets/furniro-syltherine.jpg";
+import furniroPotty from "../furniro_assets/furniro-potty.jpg";
+import furniroGrifo from "../furniro_assets/furniro-grifo.jpg";
+import furniroLeviosa from "../furniro_assets/furniro-leviosa.jpg";
+import furniroRespira from "../furniro_assets/furniro-respira.jpg";
+import "../furniro_components/OurProductsStyle.css";
+function OurProductsCard({
+  productImage,
+  productTitle,
+  productSubtitle,
+  productSalePrice,
+  productOriginalPrice,
+  productSaleRateBalloon,
+}) {
   return (
     <>
-      <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+      <div
+        className="card "
+        style={{ width: "19rem", height: "auto", borderRadius: "0px" }}
+      >
+        <div>{productSaleRateBalloon}</div>
+        <img src={productImage} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{productTitle}</h5>
+          <p
+            className="card-text"
+            style={{ color: "#898989", fontSize: "1rem", fontWeight: "500" }}
+          >
+            {productSubtitle}
+          </p>
+          <div className="card-prices-container d-flex gap-2 justify-content-start align-items-center mx-0">
+            <p
+              className="card-text m-0"
+              style={{
+                color: "#333333",
+                fontSize: "1.25rem",
+                fontWeight: "500",
+              }}
+            >
+              {productSalePrice}
+            </p>
+            <p
+              className="card-text"
+              style={{
+                color: "#898989",
+                fontSize: "1rem",
+                fontWeight: "500",
+              }}
+            >
+              <del>{productOriginalPrice}</del>
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
@@ -26,7 +63,51 @@ function OurProductsCard({productTitle, productSubtitle, productSalePrice, produ
 function OurProducts() {
   return (
     <>
-      <div className="container-fluid"></div>
+      <div className="container-fluid " id="our-product-parent-container">
+        <header className="d-flex justify-content-center px-4 py-4 py-md-5 px-md-5">
+          <h1>Our Products</h1>
+        </header>
+        <div className="container d-flex justify-content-center">
+          <div className="row d-lg-flex justify-content-center">
+            <div className="col-12 col-lg-4 col-xl-3  col-md-6 d-flex justify-content-center d-lg-block p-lg-2 my-3">
+              <OurProductsCard
+                productImage={furniroSyltherine}
+                productTitle="Syltherine"
+                productSubtitle="Stylish Cafe Chair"
+                productSalePrice="PHP 1,666.399"
+                productOriginalPrice="PHP 2,332.958"
+              />
+            </div>
+            <div className="col-12 col-lg-4 col-xl-3  col-md-6 d-flex justify-content-center d-lg-block p-lg-2 my-3">
+              <OurProductsCard
+                productImage={furniroSyltherine}
+                productTitle="Syltherine"
+                productSubtitle="Stylish Cafe Chair"
+                productSalePrice="PHP 1,666.399"
+                productOriginalPrice="PHP 2,332.958"
+              />
+            </div>
+            <div className="col-12 col-lg-4 col-xl-3  col-md-6 d-flex justify-content-center d-lg-block p-lg-2 my-3">
+              <OurProductsCard
+                productImage={furniroSyltherine}
+                productTitle="Syltherine"
+                productSubtitle="Stylish Cafe Chair"
+                productSalePrice="PHP 1,666.399"
+                productOriginalPrice="PHP 2,332.958"
+              />
+            </div>
+            <div className="col-12 col-lg-4 col-xl-3  col-md-6 d-flex justify-content-center d-lg-block p-lg-2 my-3">
+              <OurProductsCard
+                productImage={furniroSyltherine}
+                productTitle="Syltherine"
+                productSubtitle="Stylish Cafe Chair"
+                productSalePrice="PHP 1,666.399"
+                productOriginalPrice="PHP 2,332.958"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
